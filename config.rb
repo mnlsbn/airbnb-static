f0bc30c495eb12dc42f5e33f3555b91a146c8ce7
manuel sabine <manuel.sabine@gmail.com>
2017-05-08 19:15:02 +0200

    fix deploy

--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@
 # Compass
 ###
 
+
+
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
@@ -28,6 +30,11 @@ activate :autoprefixer
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+data.flats.keys.each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
+
+
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
